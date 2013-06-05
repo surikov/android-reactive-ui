3e26a66e37916fc9e0e3d96ef17b9c3f58050891
sss1024@gmail.com <sss1024@gmail.com@918898bd-0fb4-e391-f412-bdf294270514>
2013-06-05 08:04:54 +0000

    107

--- src/reactive/ui/Demo.java
+++ src/reactive/ui/Demo.java
@@ -42,7 +42,7 @@ import java.text.*;
 public class Demo extends Activity {
 	Layoutless layoutless;
 	public static String version() {
-		return "1.06";
+		return "1.07";
 	}
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
